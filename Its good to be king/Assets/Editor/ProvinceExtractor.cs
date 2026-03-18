00d2d1ac15268fd1080e2e2e007bc57aa790f846
Olkan-Delux <oliwer.allden@gmail.com>
2026-03-18 18:50:42 +0100

    Lite map stuff

---
+++ Its good to be king/Assets/Editor/ProvinceExtractor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ProvinceExtractor
+{
+    [MenuItem("Tools/Generate Provinces")]
+    static void Generate()
+    {
+        //GameObject map = Selection.activeObject as GameObject;
+        //SpriteRenderer sprite = map.GetComponent<SpriteRenderer>();
+        Texture2D map = Selection.activeObject as Texture2D;
+
+        HashSet<Color32> colors = new HashSet<Color32>();
+        colors.Clear();
+
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                colors.Add(map.GetPixel(x, y));
+            }
+        }
+
+        Debug.Log("Found provinces: " + colors.Count);
+
+        foreach (var c in colors)
+        {
+            Debug.Log(c);
+        }
+    }
+}
---
+++ Its good to be king/Assets/Scripts/MapSelectorScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSelectorScript : MonoBehaviour
+{
+    private Sprite myColorMap;
+    private Dictionary<Color, County> Counties = new Dictionary<Color, County>();
+
+    private void Update()
+    {
+        
+    }
+}
